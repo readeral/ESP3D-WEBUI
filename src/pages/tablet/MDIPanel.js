c302b57b6fc632afcbef80c318e86f8c9590a5bf
readeral <alan@readeral.com>
2022-08-19 16:38:48 +1000

    refactor to lift state up, enable running of gcode

--- src/pages/tablet/MDIPanel.js
+++ src/pages/tablet/MDIPanel.js
@@ -8,30 +8,21 @@ import { useUiContext, useUiContextFn } from "../../contexts"
 import { T } from "../../components/Translations"
 import { files, processor } from "../../targets"
 
-let currentFS = "DIRECTSD"
-const currentPath = {}
-const filesListCache = {}
+const MDIpanel = (props) => {
+    const {
+        fileref,
+        filesSelected,
+        filesList,
+        selectedFile,
+        handleSelectChange,
+        openFileUploadBrowser,
+        readFile,
+        gcodePreview,
+        sendCommand
+    } = props;
 
-const MDIpanel = ({sendCommand, sendSerialCmd, setGCodeLoaded, gCodeLoaded}) => {
-    const [isLoading, setIsLoading] = useState(false)
-    const [filesList, setFilesList] = useState(filesListCache[currentFS])
-    const { createNewRequest, abortRequest } = useHttpFn
-    const { modals, toasts } = useUiContext()
-    const fileref = useRef()
-    const progressBar = {}
     const MDI1 = useRef(null);
     const MDI2 = useRef(null);
-    const [selectedFile, setSelectedFile] = useState(null)
-    const [gcodePreview, setGcodePreview] = useState('')
-
-    const onCancel = () => {
-        useUiContextFn.haptic()
-        processor.stopCatchResponse()
-        setIsLoading(false)
-        toasts.addToast({ content: T("S175"), type: "error" })
-        filesListCache[currentFS] = { files: [], status: "S22" }
-        setFilesList(filesListCache[currentFS])
-    }
 
     // Send an MDI command when clicking either MDI button
     function handleMDIcommand(ref) {
@@ -50,229 +41,6 @@ const MDIpanel = ({sendCommand, sendSerialCmd, setGCodeLoaded, gCodeLoaded}) => 
             sendCommand(e.target.value)
         }
       };
-
-    // Uploads files to filesystem (currently only SD card)
-    const uploadFiles = () => {
-        setIsLoading(true)
-        const cmd = files.command(currentFS, "upload", currentPath[currentFS])
-        const list = fileref.current.files
-        if (list.length > 0) {
-            showProgressModal({
-                modals,
-                title: T("S32"),
-                button1: {
-                    cb: abortRequest,
-                    text: T("S28"),
-                },
-                content: <Progress progressBar={progressBar} max="100" />,
-            })
-            //prepare POST data
-            const formData = new FormData()
-            formData.append("path", currentPath[currentFS])
-            for (let i = 0; i < list.length; i++) {
-                const file = list[i]
-                const arg =
-                    currentPath[currentFS] +
-                    (currentPath[currentFS] == "/" ? "" : "/") +
-                    file.name +
-                    "S"
-                //append file size first to check updload is complete
-                formData.append(arg, file.size)
-                formData.append(
-                    "myfiles",
-                    file,
-                    currentPath[currentFS] +
-                        (currentPath[currentFS] == "/" ? "" : "/") +
-                        file.name
-                )
-            }
-            //now do request
-            createNewRequest(
-                espHttpURL(cmd.url),
-                { method: "POST", id: "upload", body: formData },
-                {
-                    onSuccess: (result) => {
-                        modals.removeModal(modals.getModalIndex("upload"))
-                        const cmdpost = files.command(
-                            currentFS,
-                            "postUpload",
-                            currentPath[currentFS],
-                            fileref.current.files[0].name
-                        )
-                        if (cmdpost.type == "error" || cmdpost.type == "none") {
-                            filesListCache[currentFS] = files.command(
-                                currentFS,
-                                "formatResult",
-                                result
-                            )
-                            setFilesList(filesListCache[currentFS])
-                            setIsLoading(false)
-                        } else {
-                            if (cmdpost.type == "refresh") {
-                                //this is needed because the board is still busy
-                                setTimeout(() => {
-                                    onRefresh(null, cmdpost.arg)
-                                }, cmdpost.timeOut)
-                            }
-                        }
-                    },
-                    onFail: (error) => {
-                        modals.removeModal(modals.getModalIndex("upload"))
-                        toasts.addToast({ content: error, type: "error" })
-                        setIsLoading(false)
-                    },
-                    onProgress: (e) => {
-                        if (
-                            progressBar.update &&
-                            typeof progressBar.update === "function"
-                        )
-                            progressBar.update(e)
-                    },
-                }
-            )
-        }
-    }
-
-    // Handles selected files and shows upload confirmation modal
-    const filesSelected = (e) => {
-        const content = []
-        const length = fileref.current.files.length
-        for (let index = 0; index < length; index++) {
-            content.push(<li>{fileref.current.files[index].name}</li>)
-            if (
-                !files.capability(
-                    currentFS,
-                    "Upload",
-                    currentPath[currentFS],
-                    fileref.current.files[index].name
-                )
-            ) {
-                const eMsg = files.capability(
-                    currentFS,
-                    "Upload",
-                    currentPath[currentFS],
-                    fileref.current.files[index].name,
-                    true
-                )
-                toasts.add({ content: T(eMsg), type: "error" })
-            }
-        }
-
-        showConfirmationModal({
-            modals,
-            title: T("S31"),
-            content: <CenterLeft>{content}</CenterLeft>,
-            button1: {
-                cb: uploadFiles,
-                text: T("S27"),
-            },
-            button2: { text: T("S28") },
-        })
-    }
-
-    // Setup for file browser window
-    const setupFileInput = () => {
-        if (currentFS == "") return
-        fileref.current.multiple = files.capability(currentFS, "UploadMultiple")
-        if (files.capability(currentFS, "UseFilters")) {
-            let f = useUiContextFn.getValue("filesfilter").trim()
-            if (f.length > 0 && f != "*") {
-                f = "." + f.replace(/;/g, ",.")
-            } else f = "*"
-            fileref.current.accept = f
-        } else {
-            fileref.current.accept = "*"
-        }
-    }
-
-    // Clicks the hidden 'file' form field
-    const openFileUploadBrowser = () => {
-        useUiContextFn.haptic()
-        fileref.current.value = ""
-        fileref.current.click()
-    }
-
-    const onRefresh = (e, usecache = false) => {
-        if (e) useUiContextFn.haptic()
-        setIsLoading(true)
-        if (usecache && filesListCache[currentFS]) {
-            if (files.capability(currentFS, "IsFlatFS")) {
-                setFilesList(
-                    files.command(
-                        currentFS,
-                        "filterResult",
-                        filesListCache[currentFS],
-                        currentPath[currentFS]
-                    )
-                )
-            } else {
-                setFilesList(filesListCache[currentFS])
-            }
-            setIsLoading(false)
-        } else {
-            const cmd = files.command(currentFS, "list", currentPath[currentFS])
-            if (cmd.type == "url") {
-                createNewRequest(
-                    espHttpURL(cmd.url, cmd.args),
-                    { method: "GET" },
-                    {
-                        onSuccess: (result) => {
-                            filesListCache[currentFS] = files.command(
-                                currentFS,
-                                "formatResult",
-                                result
-                            )
-                            setFilesList(filesListCache[currentFS])
-                            setIsLoading(false)
-                        },
-                        onFail: (error) => {
-                            console.log(error)
-                            setIsLoading(false)
-                            toasts.addToast({ content: error, type: "error" })
-                        },
-                    }
-                )
-            } else if (cmd.type == "cmd") {
-                if (
-                    processor.startCatchResponse(
-                        currentFS,
-                        "list",
-                        processFeedback
-                    )
-                )
-                    sendSerialCmd(cmd.cmd)
-            }
-        }
-    }
-
-    // Instantiate the file upload/load system
-    useEffect(() => {
-        setGCodeLoaded(false);
-        setupFileInput();
-        if (!currentPath[currentFS]) {
-            currentPath[currentFS] = "/"
-        }
-        onRefresh(null, 'init');
-        console.log(filesListCache)
-    }, [])
-
-    useEffect(() => {
-        console.log(currentPath[currentFS]);
-        console.log(filesList)
-    }, [filesList])
-
-    function readFile(file) {
-        const fetchedFile = file ? file : selectedFile;
-        fetch(encodeURI('SD/' + fetchedFile))
-        .then(response => response.text() )
-        .then(gcode => setGcodePreview(gcode) );
-        setGCodeLoaded(true);
-    }
-
-    function handleSelectChange(event) {
-        setSelectedFile(event.target.value);
-        readFile(event.target.value);
-    }
 
     return (
         <Fragment>
--- src/pages/tablet/index.js
+++ src/pages/tablet/index.js
@@ -1,6 +1,6 @@
 
 import { h } from "preact"
-import { useEffect, useState } from "preact/hooks"
+import { useEffect, useState, useRef } from "preact/hooks"
 import { useHttpFn } from "../../hooks"
 
 import TabletNav from "./tabletNav";
@@ -10,16 +10,27 @@ import MDIpanel from "./MDIPanel";
 
 import { espHttpURL, replaceVariables } from "../../components/Helpers"
 import { useUiContext, useUiContextFn } from "../../contexts"
-import { useTargetContext, variablesList } from "../../targets"
+import { files, processor, useTargetContext, variablesList } from "../../targets"
+
+let currentFS = "DIRECTSD"
+const currentPath = {}
+const filesListCache = {}
 
 const Tablet = () => {
-    const { modals, toasts } = useUiContext()
-    const { createNewRequest } = useHttpFn;
     const targetContext = useTargetContext();
+    const { modals, toasts } = useUiContext()
+    const { createNewRequest, abortRequest } = useHttpFn
+
+    const fileref = useRef()
 
     const [units, setUnits] = useState('mm')
     const [gCodeLoaded, setGCodeLoaded] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const [filesList, setFilesList] = useState(filesListCache[currentFS])
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [gcodePreview, setGcodePreview] = useState('')
 
+    const progressBar = {}
     let currentFeedRate = {};
 
     function startGcode() {
@@ -31,11 +42,244 @@ const Tablet = () => {
             currentPath[
                 currentFS
             ],
-            line.name
+            selectedFile
         )
         sendSerialCmd(
             cmd.cmd
         )
+    }
+
+    const onCancel = () => {
+        useUiContextFn.haptic()
+        processor.stopCatchResponse()
+        setIsLoading(false)
+        toasts.addToast({ content: T("S175"), type: "error" })
+        filesListCache[currentFS] = { files: [], status: "S22" }
+        setFilesList(filesListCache[currentFS])
+    }
+
+
+    // Uploads files to filesystem (currently only SD card)
+    const uploadFiles = () => {
+        setIsLoading(true)
+        const cmd = files.command(currentFS, "upload", currentPath[currentFS])
+        const list = fileref.current.files
+        if (list.length > 0) {
+            showProgressModal({
+                modals,
+                title: T("S32"),
+                button1: {
+                    cb: abortRequest,
+                    text: T("S28"),
+                },
+                content: <Progress progressBar={progressBar} max="100" />,
+            })
+            //prepare POST data
+            const formData = new FormData()
+            formData.append("path", currentPath[currentFS])
+            for (let i = 0; i < list.length; i++) {
+                const file = list[i]
+                const arg =
+                    currentPath[currentFS] +
+                    (currentPath[currentFS] == "/" ? "" : "/") +
+                    file.name +
+                    "S"
+                //append file size first to check updload is complete
+                formData.append(arg, file.size)
+                formData.append(
+                    "myfiles",
+                    file,
+                    currentPath[currentFS] +
+                        (currentPath[currentFS] == "/" ? "" : "/") +
+                        file.name
+                )
+            }
+            //now do request
+            createNewRequest(
+                espHttpURL(cmd.url),
+                { method: "POST", id: "upload", body: formData },
+                {
+                    onSuccess: (result) => {
+                        modals.removeModal(modals.getModalIndex("upload"))
+                        const cmdpost = files.command(
+                            currentFS,
+                            "postUpload",
+                            currentPath[currentFS],
+                            fileref.current.files[0].name
+                        )
+                        if (cmdpost.type == "error" || cmdpost.type == "none") {
+                            filesListCache[currentFS] = files.command(
+                                currentFS,
+                                "formatResult",
+                                result
+                            )
+                            setFilesList(filesListCache[currentFS])
+                            setIsLoading(false)
+                        } else {
+                            if (cmdpost.type == "refresh") {
+                                //this is needed because the board is still busy
+                                setTimeout(() => {
+                                    onRefresh(null, cmdpost.arg)
+                                }, cmdpost.timeOut)
+                            }
+                        }
+                    },
+                    onFail: (error) => {
+                        modals.removeModal(modals.getModalIndex("upload"))
+                        toasts.addToast({ content: error, type: "error" })
+                        setIsLoading(false)
+                    },
+                    onProgress: (e) => {
+                        if (
+                            progressBar.update &&
+                            typeof progressBar.update === "function"
+                        )
+                            progressBar.update(e)
+                    },
+                }
+            )
+        }
+    }
+
+    // Handles selected files and shows upload confirmation modal
+    const filesSelected = (e) => {
+        const content = []
+        const length = fileref.current.files.length
+        for (let index = 0; index < length; index++) {
+            content.push(<li>{fileref.current.files[index].name}</li>)
+            if (
+                !files.capability(
+                    currentFS,
+                    "Upload",
+                    currentPath[currentFS],
+                    fileref.current.files[index].name
+                )
+            ) {
+                const eMsg = files.capability(
+                    currentFS,
+                    "Upload",
+                    currentPath[currentFS],
+                    fileref.current.files[index].name,
+                    true
+                )
+                toasts.add({ content: T(eMsg), type: "error" })
+            }
+        }
+
+        showConfirmationModal({
+            modals,
+            title: T("S31"),
+            content: <CenterLeft>{content}</CenterLeft>,
+            button1: {
+                cb: uploadFiles,
+                text: T("S27"),
+            },
+            button2: { text: T("S28") },
+        })
+    }
+
+    // Setup for file browser window
+    const setupFileInput = () => {
+        if (currentFS == "") return
+        fileref.current.multiple = files.capability(currentFS, "UploadMultiple")
+        if (files.capability(currentFS, "UseFilters")) {
+            let f = useUiContextFn.getValue("filesfilter").trim()
+            if (f.length > 0 && f != "*") {
+                f = "." + f.replace(/;/g, ",.")
+            } else f = "*"
+            fileref.current.accept = f
+        } else {
+            fileref.current.accept = "*"
+        }
+    }
+
+    // Clicks the hidden 'file' form field
+    const openFileUploadBrowser = () => {
+        useUiContextFn.haptic()
+        fileref.current.value = ""
+        fileref.current.click()
+    }
+
+    const onRefresh = (e, usecache = false) => {
+        if (e) useUiContextFn.haptic()
+        setIsLoading(true)
+        if (usecache && filesListCache[currentFS]) {
+            if (files.capability(currentFS, "IsFlatFS")) {
+                setFilesList(
+                    files.command(
+                        currentFS,
+                        "filterResult",
+                        filesListCache[currentFS],
+                        currentPath[currentFS]
+                    )
+                )
+            } else {
+                setFilesList(filesListCache[currentFS])
+            }
+            setIsLoading(false)
+        } else {
+            const cmd = files.command(currentFS, "list", currentPath[currentFS])
+            if (cmd.type == "url") {
+                createNewRequest(
+                    espHttpURL(cmd.url, cmd.args),
+                    { method: "GET" },
+                    {
+                        onSuccess: (result) => {
+                            filesListCache[currentFS] = files.command(
+                                currentFS,
+                                "formatResult",
+                                result
+                            )
+                            setFilesList(filesListCache[currentFS])
+                            setIsLoading(false)
+                        },
+                        onFail: (error) => {
+                            console.log(error)
+                            setIsLoading(false)
+                            toasts.addToast({ content: error, type: "error" })
+                        },
+                    }
+                )
+            } else if (cmd.type == "cmd") {
+                if (
+                    processor.startCatchResponse(
+                        currentFS,
+                        "list",
+                        processFeedback
+                    )
+                )
+                    sendSerialCmd(cmd.cmd)
+            }
+        }
+    }
+
+    // Instantiate the file upload/load system
+    useEffect(() => {
+        setGCodeLoaded(false);
+        setupFileInput();
+        if (!currentPath[currentFS]) {
+            currentPath[currentFS] = "/"
+        }
+        onRefresh(null, 'init');
+        console.log(filesListCache)
+    }, [])
+
+    useEffect(() => {
+        console.log(currentPath[currentFS]);
+        console.log(filesList)
+    }, [filesList])
+
+    function readFile(file) {
+        const fetchedFile = file ? file : selectedFile;
+        fetch(encodeURI('SD/' + fetchedFile))
+        .then(response => response.text() )
+        .then(gcode => setGcodePreview(gcode) );
+        setGCodeLoaded(true);
+    }
+
+    function handleSelectChange(event) {
+        setSelectedFile(event.target.value);
+        readFile(event.target.value);
     }
 
     useEffect(() => { //@todo does this need changing to allow the feedrate to be updated? as per XY example below
@@ -172,9 +416,14 @@ const Tablet = () => {
                 units={units}/>
             <MDIpanel
                 sendCommand={sendCommand}
-                sendSerialCmd={sendSerialCmd}
-                setGCodeLoaded={setGCodeLoaded}
-                gCodeLoaded={gCodeLoaded}
+                fileref={fileref}
+                filesList={filesList}
+                filesSelected={filesSelected}
+                selectedFile={selectedFile}
+                handleSelectChange={handleSelectChange}
+                openFileUploadBrowser={openFileUploadBrowser}
+                readFile={readFile}
+                gcodePreview={gcodePreview}
             />
         </div>
     )
